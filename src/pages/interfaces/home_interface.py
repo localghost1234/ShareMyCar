219a0568bcab454f4bca0cd6f16278dcf0d9f90b
Local Ghost <viczaz99@hotmail.com>
2025-03-12 18:14:54 -0700

    added more padding to HomeInterface

--- src/pages/interfaces/home_interface.py
+++ src/pages/interfaces/home_interface.py
@@ -5,5 +5,5 @@ class HomeInterface(BaseInterface):
     def __init__(self, root, system):
         super().__init__(root,  system, "SHARE MY CAR")
 
-        tk.Label(self.frame, text="Welcome to the car sharing management system!", font=("Helvetica", 15, "bold")).pack(pady=20)
-        tk.Label(self.frame, text="Please select any operation you wish to perform", font=("Helvetica", 14)).pack()
+        tk.Label(self.frame, text="Welcome to the car sharing management system!", font=("Helvetica", 15, "bold")).pack(pady=50)
+        tk.Label(self.frame, text="Please select any operation you wish to perform", font=("Helvetica", 14)).pack(pady=10)
