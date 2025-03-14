3ee7cad8ae53719f05953de790229a59e2e03f5d
localghost1234 <viczaz99@hotmail.com>
2025-03-13 18:04:19 -0700

    reverted refactoring in app.py

--- src/pages/app.py
+++ src/pages/app.py
@@ -1,4 +1,6 @@
+# app.py
 import tkinter as tk
+
 from src.core.system import System
 from src.pages.interfaces.home_interface import HomeInterface
 from src.misc.constants import INTERFACES_LIST
@@ -12,11 +14,16 @@ class App:
         self.root.geometry("720x480")
         self.root.title("Share My Car")
 
-        button_frame = tk.Frame(self.root).pack()
-        [tk.Button(button_frame, text=name, command=lambda c=cls: self.switch_interface(c)).pack(side=tk.LEFT) for name, cls in INTERFACES_LIST]
+        button_frame = tk.Frame(self.root)
+        button_frame.pack()
 
+        for interface_name, interface_class in INTERFACES_LIST:
+            tk.Button(button_frame, text=interface_name, command=lambda i=interface_class: self.switch_interface(i)).pack(side=tk.LEFT)
+
+        # Start with the HomeInterface
         self.switch_interface(HomeInterface)
 
+        # Register on_close() as the handler for the window close event
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
         self.root.mainloop()
@@ -24,9 +31,11 @@ class App:
     def switch_interface(self, interface_class):
         if self.current_interface:
             self.current_interface.frame.destroy()
+
         self.current_interface = interface_class(self.root, self.system)
 
     def on_close(self):
         print('Shutting down.')
+        
         self.system.close()
         self.root.destroy()
